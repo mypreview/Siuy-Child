d9732f130c4792bcd0a60b5a396f83f1a51fec9d
Mahdi Yazdani <mahdiyazdani@mail.com>
2018-03-14 14:52:25 +0400

    Create siuy-child.js

---
+++ assets/js/siuy-child.js
@@ -0,0 +1,11 @@
+/**
+ * Siuy Child Theme
+ *
+ * @author      Mahdi Yazdani
+ * @package     Siuy Child
+ * @since       1.0.0
+ */
+jQuery(document).ready(function($) {
+    'use strict';
+
+}); /*Document Ready End*/
